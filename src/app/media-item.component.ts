766db7760a1049f498592b1dca2dcb5706a80347
Kate Skorija <kateskorija@gmail.com>
2020-10-22 13:22:47 -0700

    use Input decorator to pass info from app component to media item component

--- src/app/media-item.component.ts
+++ src/app/media-item.component.ts
@@ -1,9 +1,16 @@
 
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'mw-media-item',
   templateUrl: './media-item.component.html',
   styleUrls: ['./media-item.component.css']
 })
-export class MediaItemComponent {}
+export class MediaItemComponent {
+
+  @Input() mediaItem;
+
+  onDelete() {
+    console.log('deleted');
+  }
+}
